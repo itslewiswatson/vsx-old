c3367f84bf9fa28fb8b07d24b5f2831ecd064970
nokizorque <noki@zorque.xyz>
2016-06-19 18:47:38 +1000

    Navbar buttons will now be highlighted according to what page a user is on

---
+++ src/js/buttons.js
@@ -0,0 +1,27 @@
+function highlighting() {
+	var a = window.location.pathname.split("/");
+	var curr = a[a.length - 1].split(".php")[0];
+	
+	// I believe this can be simplified
+	/*
+	if (window.location.pathname.match("stocks")) {
+		document.getElementById("index").className += " active";
+		return;
+	}
+	if (curr == "") {
+		document.getElementById("index").className += " active";
+		return;
+	}
+	*/
+	
+	if (window.location.pathname.match("stocks")) {
+		curr = "stocks";
+	}
+	if (curr == "") {
+		curr = "index";
+	}
+	
+	document.getElementById(curr).className += " active";
+}
+//window.onload = highlighting; // For some reason, this does not work (thanks JavaScript)
+highlighting();
