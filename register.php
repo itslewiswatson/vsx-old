3af6fd11a571f5245b04c8b5fdd4b29b879435c6
nokizorque <noki@zorque.xyz>
2016-06-19 12:29:25 +1000

    Added proper registration handling Passwords are now hashed in bcrypt Fixed some templating errors Fixed forms not being submitted properly

--- register.php
+++ register.php
@@ -1,13 +1,11 @@
 <?php
 	include "/src/templates/header.html";
 	require "core.php";
-	include "/src/templates/register.html";
-	
-	function isAccount($usr, $email) {
+		
+	function isAccount($usr = NULL, $email = NULL) {
 		global $db;
 		$queryString = "";
 		
-		// If there is no email (emails are optional, for now)
 		if (!$email || $email == "" || $email == NULL) {
 			$queryString = "SELECT usr, email FROM accounts WHERE usr = '" . $usr . "'";
 		}
@@ -20,14 +18,59 @@
 		$res_usr = $result["usr"];
 		$res_email = $result["email"];
 		
-		if ($res_usr == NULL) {
+		if ($res_usr !== NULL) {
 			return array(true, "An account with this username already exists");
 		}
-		elseif ($res_email == NULL) {
+		elseif ($res_email !== NULL) {
 			return array(true, "An account with this email already exists");
 		}
 		
 		return array(false, "");
+	}
+	
+	function register($usr, $email, $passwd) {
+		global $db;
+		
+		if (isAccount($usr)[1] === false) {
+			return isAccount($usr)[1];
+		}
+		if (isAccount($email)[1] === false) {
+			return isAccount(NULL, $email)[1];
+		}
+		
+		$passwd = password_hash($passwd, PASSWORD_BCRYPT);
+		
+		$statement = $db->prepare("INSERT INTO accounts (usr, email, passwd) VALUES (?, ?, ?)");
+		$statement->bind_param("sss", $usr, $email, $passwd);
+		$statement->execute();
+		
+		return true;
+	}
+	
+	if (isset($_POST["username"])) {
+		$usr = $_POST["username"];
+		$email = $_POST["email"];
+		$passwd = $_POST["passwd"];
+		$passwd_ = $_POST["passwd_confirm"];
+		
+		$regis = register($usr, $email, $passwd);
+		if ($regis !== true) {
+			// Tell them the error
+			?>
+			<div class="text-center">
+				<font color="red"><h5><?php var_dump($regis); ?></h5></font>
+				<hr>
+			</div>
+			<?php
+			include "/src/templates/register.html";
+		}
+		else {
+			// Redirect them to the login page
+			header("Location: login.php");
+		}
+	}
+	else {
+		include "/src/templates/register.html";
 	}
 	
 	include "/src/templates/footer.html";
