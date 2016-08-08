1d31f14b2239d4ba44f87474281e89c4120ddff7
nokizorque <noki@zorque.xyz>
2016-08-08 21:59:35 +1000

    Altered the way the navbar and footer are loaded on the page

--- core.php
+++ core.php
@@ -5,31 +5,46 @@
 
 	session_start();
 
+	ob_start();
+	include "src/templates/header.html";
+
 	global $db;
 	$db = new mysqli("localhost", "root", "", "vsx");
 	if (!$db) {
 		die("Failed to connect to the database");
 	}
 
+	function _header() {
+		return;
+	}
+	function _footer() {
+		return;
+	}
+
 	function buttons() {
-	?>
-	<script type="text/javascript">
-		window.onload = function () {
-			document.getElementById("right-navbar").innerHTML = "";
-			var u = "" + String(<?php isset($_SESSION["usr"]) ? $s = "'" . $_SESSION["usr"] . "'" : $s = "'N/A'"; echo strval($s); ?>) + "";
-			var loggedIn = <?php isset($_SESSION["usr"]) ? $s = 1 : $s = 0; echo $s; ?>;
-			var list = "";
-			if (loggedIn == 0) {
-				list += '<li id="register_btn"><a href="register.php">Register</a></li>';
-				list += '<li id="login_btn"><a href="login.php">Login</a></li>';
+		?>
+		<script type="text/javascript">
+			function footer() {
+				document.body.insertAdjacentHTML('beforeend', (<?php echo ("'" . str_replace("<br>", "", str_replace("\r", "", str_replace("\n", "", file_get_contents("src/templates/footer.html", true)))) . "'"); ?>));
 			}
-			else if (loggedIn == 1) {
-				list += '<li id="logged_in_btn" class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">' + "" + u + "" + '<span class="caret"></span></a><ul class="dropdown-menu" role="menu"><li><a href="#">Profile</a></li><li><a href="#">Settings</a></li><li class="divider"></li><li><a href="logout.php">Sign out</a></li></ul></li>';
+
+			window.onload = function () {
+				document.getElementById("right-navbar").innerHTML = "";
+				var u = "" + String(<?php isset($_SESSION["usr"]) ? $s = "'" . $_SESSION["usr"] . "'" : $s = "'N/A'"; echo strval($s); ?>) + "";
+				var loggedIn = <?php isset($_SESSION["usr"]) ? $s = 1 : $s = 0; echo $s; ?>;
+				var list = "";
+				if (loggedIn == 0) {
+					list += '<li id="register_btn"><a href="register.php">Register</a></li>';
+					list += '<li id="login_btn"><a href="login.php">Login</a></li>';
+				}
+				else if (loggedIn == 1) {
+					list += '<li id="logged_in_btn" class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">' + "" + u + "" + '<span class="caret"></span></a><ul class="dropdown-menu" role="menu"><li><a href="#">Profile</a></li><li><a href="#">Settings</a></li><li class="divider"></li><li><a href="logout.php">Sign out</a></li></ul></li>';
+				}
+				document.getElementById("right-navbar").innerHTML = list;
+				footer();
 			}
-			document.getElementById("right-navbar").innerHTML = list;
-		}
-	</script>
-	<?php
+		</script>
+		<?php
 	}
 	buttons();
 
@@ -43,10 +58,6 @@
 		return isset($_SESSION["usr"]) ? true : false;
 	}
 
-	function _footer() {
-		include "src/templates/footer.html";
-	}
-	function _header() {
-		include "src/templates/header.html";
-	}
+	//include "src/templates/footer.html";
+	ob_end_flush();
 ?>
