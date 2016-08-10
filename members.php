6a6ef0cdc62c87213a2ee761168e3e78ff29b56e
nokizorque <noki@zorque.xyz>
2016-08-10 19:40:10 +1000

    Syntax, styling, logic and small bug fixes

--- members.php
+++ members.php
@@ -33,15 +33,21 @@
 		</body>
 	<?php
 
-	$fields = array("usr", "DATE_FORMAT(registered_on, '%d-%m-%Y') AS registered_on", "DATE_FORMAT(last_visited, '%d-%m-%Y') AS last_visited", "name", "website", "COUNT(*) AS row_count");
-	$default["query_string"] = "SELECT " . implode(", ", $fields) . " FROM users LIMIT 100";
+	$fields = array(
+		"usr",
+		"DATE_FORMAT(registered_on, '%d-%m-%Y') AS registered_on",
+		"DATE_FORMAT(last_visited, '%d-%m-%Y') AS last_visited",
+		"name",
+		"website",
+	);
+	$default["query_string"] = "SELECT " . implode(", ", $fields) . " FROM users GROUP BY usr";
 
 	$queryString = $default["query_string"];
 
-	if ($_GET && $_GET["q"] && $_GET["q"]) {
+	if ($_GET && $_GET["q"] && isset($_GET["q"])) {
 		$i = $_GET["q"];
 		$i = str_clean($i);
-		$queryString = "SELECT " . implode(", ", $fields) . " FROM users WHERE usr LIKE '%" . $i . "%' OR email LIKE '%" . $i . "%'";
+		$queryString = "SELECT " . implode(", ", $fields) . " FROM users WHERE usr LIKE '%" . $i . "%' OR email LIKE '%" . $i . "'% OR name LIKE '%" . $i . "%'";
 	}
 
 	$query = $db->query($queryString);
@@ -56,14 +62,16 @@
 		?>
 			<title>Members - VSX</title>
 		<?php
+		$countString = "SELECT COUNT(*) AS row_count FROM users";
 	}
 	else {
 		?>
 			<title>Search Results - VSX</title>
 		<?php
+		$countString = "SELECT COUNT(*) AS row_count FROM users WHERE usr LIKE '%" . $i . "%' OR email LIKE '%" . $i . "'% OR name LIKE %'" . $i . "%'";
 	}
 
-    //echo "Showing " . $query->fetch_assoc()["row_count"] . " of " . $query->num_rows;
+    $row_count = $db->query($countString)->fetch_assoc()["row_count"];
 
 	?>
 		<html>
@@ -79,19 +87,20 @@
     							<th>Registered on</th>
     						</tr>
     						<?php
-    						while ($row = $query->fetch_assoc()) {
-    							$website = "<a href='" . $row["website"] . "' target='_blank'>" . $row["website"] . "</a>";
-    							if (!$row["website"] || !isset($row["website"])) {
-    								$website = "";
-    							}
-    							echo "<tr>
-    								<td><a href='profile.php?u=" . $row["usr"] ."'>" . $row["usr"] . "</a></td>
-    								<td>" . $row["name"] . "</td>
-    								<td>" . $website . "</td>
-    								<td>" . $row["last_visited"] . "</td>
-    								<td>" . $row["registered_on"] . "</td>
-    							</tr>";
-    						}
+    							echo "Showing " . $query->num_rows . " of " . $row_count;
+        						while ($row = $query->fetch_assoc()) {
+        							$website = "<a href='" . $row["website"] . "' target='_blank'>" . $row["website"] . "</a>";
+        							if (!$row["website"] || !isset($row["website"])) {
+        								$website = "";
+        							}
+        							echo "<tr>
+        								<td><a href='profile.php?u=" . $row["usr"] ."'>" . $row["usr"] . "</a></td>
+        								<td>" . $row["name"] . "</td>
+        								<td>" . $website . "</td>
+        								<td>" . $row["last_visited"] . "</td>
+        								<td>" . $row["registered_on"] . "</td>
+        							</tr>";
+        						}
     						?>
     					</table>
                     </div>
