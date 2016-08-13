87334ccf7d31659c7cc423b35742851235504159
nokizorque <noki@zorque.xyz>
2016-08-13 19:06:22 +1000

    Added handling for countries, added recent activity (max 3), added current value of all shares, added the thumbnail and avatar to their appropriate CSS classes, added email and flags

--- profile.php
+++ profile.php
@@ -36,9 +36,9 @@
     }
 
     $profile = str_clean($profile);
-	
+
 	// Actual SQL queries
-	
+
 	// The main one
 	$fields = array(
 		"DATE_FORMAT(registered_on, '%l:%i %p ─ %d.%m.%Y') AS registered_on",
@@ -47,20 +47,25 @@
 		"website",
 		"avatar",
 		"bio",
+        "email",
+        "ip"
 	);
     $userData = $db->query(
-		"SELECT " . implode(", ", $fields) . " 
+		"SELECT " . implode(", ", $fields) . "
 		FROM users
 		WHERE usr = '" . $profile . "'"
 	);
 	// Don't do the extra queries if they don't need to be done
+    if (!$userData) {
+        echo "VSX has encountered an internal error";
+        exit;
+    }
 	if ($userData->num_rows == 0) {
         echo "We could not find a user with this name";
-        _footer();
         exit;
     }
     $result = $userData->fetch_assoc();
-	
+
 	// User's stock count
 	$q1 = $db->query(
 		"SELECT COUNT(*) AS stock_count
@@ -68,16 +73,15 @@
 		WHERE usr = '" . $profile . "'"
 	);
 	$stock_count = $q1->fetch_assoc()["stock_count"];
-	
-	
+
 	// User's total shares
 	$q2 = $db->query(
 		"SELECT SUM(amount) AS amount
 		FROM stocks__holders
 		WHERE usr = '" . $profile . "'"
-	);	
+	);
 	$amount = $q2->fetch_assoc()["amount"];
-	
+
 	// Total shares
 	/*
 	$q3 = $db->query(
@@ -93,7 +97,7 @@
 	if ($total_amount == 0) {
 		$total_amount = 1;
 	}
-	
+
 	// If they own shares, display a random one with its information
 	if ($amount > 0) {
 		$q4 = $db->query(
@@ -108,7 +112,7 @@
 		$random_amount = $random["amount"];
 		$random_stock = $random["stock"];
 		$random_total = $random["total_options"];
-		
+
 		// Query for the total options available of the specified random stock
 		/*
 		$q5 = $db->query(
@@ -119,7 +123,7 @@
 		$random_total = $q5->fetch_assoc()["total_options"];
 		*/
 	}
-	
+
 	// Total investments
 	$q6 = $db->query(
 		"SELECT SUM(total_price) AS price
@@ -129,23 +133,50 @@
 		LIMIT 1"
 	);
 	$total_investments = $q6->fetch_assoc()["price"];
-	
+
+    // Recent activity
+    // Checks for activity in past 3 days
+    $q7 = $db->query(
+        "SELECT stock, qty, action
+        FROM stocks__transactions
+        WHERE usr = '" . $profile . "'
+        AND (UNIX_TIMESTAMP(DATE(timing)) - UNIX_TIMESTAMP()) <= 259200
+        LIMIT 3"
+    );
+    //$recent_activity =
+
+    // Current value of all stocks they own
+    $q8 = $db->query(
+        "SELECT SUM(qty * price) AS val
+        FROM stocks__history, stocks__transactions
+        WHERE usr = '" . $profile . "'
+        AND stocks__history.stock = stocks__transactions.stock
+        AND stocks__history.timing = (
+            SELECT MAX(stocks__history.timing)
+            FROM stocks__transactions, stocks__history
+            WHERE usr = '" . $profile . "'
+            AND stocks__transactions.stock = stocks__history.stock
+        )
+        LIMIT 1"
+    );
+    $total_value = $q8->fetch_assoc()["val"];
+
+    // Get profile's IP
+    @$countryData = file_get_contents("http://freegeoip.net/json/" . $result["ip"]);
+    @$countryData = json_decode($countryData, true); // Converts the JSON string to an array
+    $countryLong = $countryData["country_name"] != NULL ? $countryData["country_name"] : "N/A";
+    $countryShort = $countryData["country_code"] != NULL ? $countryData["country_code"] : "N/A";
+
     ?>
         <html>
-            <style type="text/css">
-            #avatar img {
-                max-width: 325px;
-                width: 150%;
-                height: auto;
-                margin: 0 auto;
-            }
-            </style>
+            <link rel="stylesheet" type="text/css" href="src/css/custom.css"/>
+            <link rel="stylesheet" type="text/css" href="src/css/flags.css"/>
             <title>Profile of <?php echo $profile; ?> - VSX</title>
             <body>
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-4" id="avatar">
-							<div class="thumbnail">
+                        <div class="col-md-4 avatar-display">
+							<div class="thumbnail profile">
 								<?php
 									if (isset($result["avatar"])) {
 									?>
@@ -188,8 +219,15 @@
                         </div>
                         <div class="col-md-8">
                             <!--<h1><?php //echo $profile; ?></h1>-->
+                            <h3>User</h3>
+                            <!-- Add option to hide email -->
+                            <p>Email: <a href=<?php echo "mailto:" . $result["email"]; ?> target="_top"><?php echo $result["email"]; ?></a></p>
+                            <!-- Add flag and country (current placeholder) -->
+                            <p>Location: <?php echo $countryLong; ?> <img src="src/images/blank.gif" class='<?php echo "flag flag-" . strtolower($countryShort) . ""; ?>'/></p>
+                            <p>Registered: <?php echo $result["registered_on"]; ?></p>
+                            <p>Last Active: <?php echo $result["last_visited"]; ?></p>
+                            <hr>
 							<h3>Stocks</h3>
-							<hr>
 							<p>Shareholder of <strong><?php echo $stock_count; ?></strong> different stock(s)</p>
                             <p>Owns <strong><?php echo round(($amount / $total_amount) * 100, 3); ?></strong>% of all stock listed on VSX</p>
                             <!-- Maybe check if a majority shareholder in one stock, and display it? -->
@@ -200,21 +238,33 @@
 									<?php
 								}
 							?>
-							<h3>Monetary</h3>
-							<hr>
-                            <p>Current Returns:</p>
-							<p>Total Invested: $<?php echo sprintf("%4.2f", $total_investments); ?></p>
-							<p>Profit Loss:</p>
-                            <h3>Activity</h3>
                             <hr>
-                            <p>Registered: <?php echo $result["registered_on"]; ?></p>
-                            <p>Last Active: <?php echo $result["last_visited"]; ?></p>
+							<h3>Monetary</h3>
+                            <p>Current Returns: $<?php echo number_format($total_value, 2); ?></p>
+							<p>Total Invested: $<?php echo number_format($total_investments, 2); ?></p>
+							<!--<p>Profit Loss:</p>-->
+                            <hr>
+                            <h3>Recent Activity</h3>
+                            <!-- If in last week or so (max to display = 3) -->
+                            <?php
+                                if ($q7->num_rows == 0) {
+                                    ?>
+                                        <p>No recent activity</p>
+                                    <?php
+                                }
+                                else {
+                                    while ($activity = $q7->fetch_assoc()) {
+                                        $action = $activity["action"] == "B" ? "purchased" : "sold";
+                                        ?>
+                                            <p>Recently <?php echo $action; ?> <strong><?php echo $activity["qty"]; ?></strong> shares of <strong><a href=<?php echo "stocks.php?stock=" . $activity["stock"]; ?> target="_blank"><?php echo $activity["stock"]; ?></a></strong></p>
+                                        <?php
+                                    }
+                                }
+                            ?>
                         </div>
                     </div>
                 </div>
             </body>
         </html>
     <?php
-
-    _footer();
 ?>
