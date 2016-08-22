eef007ae7c81a34d9b25ca5a861bb052ba802c08
nokizorque <noki@zorque.xyz>
2016-08-22 16:03:08 +1000

    Added a function to purchase stock and grouped by stock in SQL queries

--- stocks_util.php
+++ stocks_util.php
@@ -44,7 +44,8 @@
                 FROM stocks__history
                 WHERE stock = '" . $stock . "'
                 )
-            GROUP BY stock"
+            GROUP BY stock
+			LIMIT 1"
         );
         $current_price = $CP->fetch_assoc()["current_price"];
         return $current_price;
@@ -67,9 +68,40 @@
                     WHERE stock = '" . $stock . "'
                     )
                 )
-            GROUP BY stock"
+            GROUP BY stock
+			LIMIT 1"
         );
         $previous_price = $PP->fetch_assoc()["previous_price"];
         return $previous_price;
     }
+
+	// Needs a can user buy stock function
+	function buyStock($usr, $stock, $qty) {
+		global $db;
+		require_once "profile_util.php";
+		// Check if they can buy stock in the first place
+		$price = $qty * getStockCurrentPrice($stock);
+		if ($price > getUserCredits($usr)) {
+			// Can't buy stock because the price is too high
+			return false;
+		}
+
+		// Check if user already downs stock
+		$check = $db->query("SELECT * FROM stocks__holders WHERE usr = '" . $usr . "' AND stock = '" . $stock . "' LIMIT 1");
+		if ($check->num_rows > 0) {
+			$queryString = "UPDATE stocks__holders SET amount = amount + ? WHERE usr = ? AND stock = ?";
+		}
+		else {
+			$queryString = "INSERT INTO stocks__holders (amount, usr, stock) VALUES (?, ?, ?)";
+		}
+		$q = $db->prepare($queryString);
+		$q->bind_param("iss", $qty, $usr, $stock);
+		$q->execute();
+
+		$transac = $db->prepare("INSERT INTO stocks__transactions (usr, stock, timing, qty, total_price, action) VALUES (?, ?, CURRENT_TIMESTAMP(), ?, ?, 'B')");
+		$transac->bind_param("ssid", $usr, $stock, $qty, $price);
+		$transac->execute();
+
+		return true;
+	}
 ?>
