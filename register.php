75cd034611661e66bbb57fa966bee4b9a3b38e28
nokizorque <noki@zorque.xyz>
2016-08-22 16:05:29 +1000

    Tampered the registration system, but it's still in non-working state :(

--- register.php
+++ register.php
@@ -8,20 +8,19 @@
 		$usr = str_clean($usr);
 
 		if (!$email || $email == "" || $email == NULL) {
-			$queryString = "SELECT usr, email FROM users WHERE usr LIKE '" . $usr . "' OR LOWER(usr) LIKE '" . $usr . "' GROUP BY usr";
+			$queryString = "SELECT usr, email FROM users WHERE usr LIKE '" . $usr . "' GROUP BY usr LIMIT 1";
 		}
 		else {
-			$queryString = "SELECT usr, email FROM users WHERE usr LIKE '" . $usr . "' OR email = '" . $email . "' GROUP BY usr";
+			$queryString = "SELECT usr, email FROM users WHERE usr LIKE '" . $usr . "' OR email = '" . $email . "' GROUP BY usr LIMIT 1";
 		}
 
 		$q = $db->query($queryString);
 		$result = $q->fetch_assoc();
-		$res_usr = $result["usr"];
-		$res_email = $result["email"];
 
-		if ($res_usr !== NULL) {
+		/*
+		if ($q->num_rows > 0) {
 			$appendage = "";
-			if ($res_usr != $usr && strtolower($res_usr) == strtolower($usr)) {
+			if (($res_usr != $usr) && (strtolower($res_usr) == strtolower($usr))) {
 				$appendage = ", in different case,";
 			}
 			return array(false, "An account with this username" . $appendage . " already exists");
@@ -29,20 +28,26 @@
 		elseif ($res_email !== NULL) {
 			return array(false, "An account with this email already exists");
 		}
+		*/
+		
+		//if ($q->num_rows > 0) {
+			return array(false, "ERROR");
+		//}
 
-		return array(true, "");
+		return array(true, "success");
 	}
 
 	function register($usr, $email, $passwd) {
 		global $db;
 
-		if (isAccount($usr)[1] == false) {
-			return isAccount($usr)[1];
+		if (isAccount($usr)[0] == false) {
+			return isAccount($usr);
 		}
-		if (isAccount($email)[1] == false) {
-			return isAccount(NULL, $email)[1];
+		if (isAccount($email)[0] == false) {
+			return isAccount(NULL, $email);
 		}
-
+		echo "PASSED CHECKS";
+		
 		$passwd = password_hash($passwd, PASSWORD_BCRYPT);
 
 		$statement = $db->prepare("INSERT INTO users (usr, email, passwd, registered_on) VALUES (?, ?, ?, CURRENT_TIMESTAMP)");
@@ -59,12 +64,21 @@
 		$passwd_ = $_POST["passwd_confirm"];
 
 		$regis = register($usr, $email, $passwd);
-		if ($regis !== true) {
+		//var_dump("REGIS -> " . $regis);
+		if ($regis[1] !== true) {
 			// Tell them the error
 			?>
 			<div class="text-center">
-				<font color="red"><h5><?php var_dump($regis); ?></h5></font>
-				<hr>
+				<body>
+				<div class="container">
+					<div class="row">
+						<div class="text-center col-sm-6 col-md-4 col-md-offset-4">
+							<!--<font color="red"></h5></font>-->
+							<div class="alert alert-danger" role="alert"><h5><?php var_dump($regis); ?></div>
+						</div>
+					</div>
+				</div>
+			</body>
 			</div>
 			<?php
 			include "src/templates/register.html";
