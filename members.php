069f5f93eee0bdcf08ea5a72ad5b2a4ebc235a78
nokizorque <noki@zorque.xyz>
2016-08-22 20:51:58 +1000

    Improved the member list and fixed search functionality within the member list

--- members.php
+++ members.php
@@ -2,37 +2,7 @@
     require_once "core.php";
 	global $db;
 
-	// Move this to a template?
-	?>
-		<body>
-			<div class="container">
-				<div class="row centered col-lg-4 col-lg-offset-4">
-						<form class="form-inline" action="members.php" method="get">
-                            <div class="form-group">
-                                <div class="input-group" style="width: 100%;">
-                        			<label class="sr-only">Search</label>
-                                    <input type="text" name="q" class="form-control" placeholder="Search for...">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-default">
-                                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                                        </button>
-                                    </span>
-                        			<!--
-                                    <span class="input-group-btn">
-                        				<button class="btn btn-default" type="submit">Search</button>
-                				    </span>
-                                    -->
-                                </div>
-                            </div>
-						</form>
-				</div>
-			</div>
-			<br>
-			<br>
-		</body>
-	<?php
-
-	$fields = array(
+    $fields = array(
 		"usr",
 		"DATE_FORMAT(registered_on, '%d-%m-%Y') AS registered_on",
 		"DATE_FORMAT(last_visited, '%d-%m-%Y') AS last_visited",
@@ -46,14 +16,59 @@
 	if ($_GET && $_GET["q"] && isset($_GET["q"])) {
 		$i = $_GET["q"];
 		$i = str_clean($i);
-		$queryString = "SELECT " . implode(", ", $fields) . " FROM users WHERE usr LIKE '%" . $i . "%' OR email LIKE '%" . $i . "'% OR name LIKE '%" . $i . "%'";
+		$queryString = "
+            SELECT " . implode(", ", $fields) . "
+            FROM users
+            WHERE usr LIKE '%" . $i . "%'
+            OR email LIKE '%" . $i . "%'
+            OR name LIKE '%" . $i . "%'
+            GROUP BY usr
+        ";
 	}
+
+	// Move this to a template?
+	?>
+		<body>
+            <br>
+			<div class="container text-center">
+				<div class="row">
+                    <div class="col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+                        <nobr><h2 style="display: inline; vertical-align: middle;">Members</h2></nobr>
+                    </div>
+    				<form class="form-inline" action="members.php" method="get">
+                        <div class="form-group pull-right">
+                            <div class="input-group">
+                    			<label class="sr-only">Search</label>
+                                <?php
+                                    if (isset($i)) {
+                                        ?>
+                                            <input type="text" name="q" class="form-control pull-right" placeholder="Search for..." value=<?php echo $i; ?>>
+                                        <?php
+                                    }
+                                    else {
+                                        ?>
+                                            <input type="text" name="q" class="form-control pull-right" placeholder="Search for...">
+                                        <?php
+                                    }
+                                ?>
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default">
+                                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+    				</form>
+                </div>
+			</div>
+			<br>
+		</body>
+	<?php
 
 	$query = $db->query($queryString);
 
 	if (!$query || $query->num_rows == 0) {
 		echo "No users found from search parameters";
-        _footer();
 		exit;
 	}
 
@@ -67,7 +82,7 @@
 		?>
 			<title>Search Results - VSX</title>
 		<?php
-		$countString = "SELECT COUNT(*) AS row_count FROM users WHERE usr LIKE '%" . $i . "%' OR email LIKE '%" . $i . "'% OR name LIKE %'" . $i . "%'";
+		$countString = "SELECT COUNT(*) AS row_count FROM users WHERE usr LIKE '%" . $i . "%' OR email LIKE '%" . $i . "%' OR name LIKE '%" . $i . "%'";
 	}
 
     $row_count = $db->query($countString)->fetch_assoc()["row_count"];
