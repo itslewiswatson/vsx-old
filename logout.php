4db536329dfdb374a766e9f992b4936269c1246f
nokizorque <noki@zorque.xyz>
2016-09-08 19:25:32 +1000

    Logout now redirects correctly

--- logout.php
+++ logout.php
@@ -1,19 +1,14 @@
 <?php
 	require_once "core.php";
 	ob_start();
-	_header();
 
 	if ($_SESSION && isset($_SESSION["usr"])) {
 		unset($_SESSION["usr"]);
+		header("Location: index.php");
 	}
 	else {
 		header("Location: index.php");
-		ob_end_flush();
-		exit;
 	}
-
 	buttons();
-
-	_footer();
 	ob_end_flush();
 ?>
