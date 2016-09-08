dfb358fae3b15ea6ee5aa14b33d57e086bc2f067
nokizorque <noki@zorque.xyz>
2016-09-08 19:25:11 +1000

    Fixed the registration system and made it so on register, a user is automatically logged in

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	require_once "core.php";
 
 	function isAccount($usr = NULL, $email = NULL) {
@@ -8,50 +8,43 @@
 		$usr = str_clean($usr);
 
 		if (!$email || $email == "" || $email == NULL) {
-			$queryString = "SELECT usr, email FROM users WHERE usr LIKE '" . $usr . "' GROUP BY usr LIMIT 1";
+			$queryString = "SELECT usr, email FROM users WHERE usr = '" . $usr . "' OR LOWER(usr) = LOWER('" . $usr . "') GROUP BY usr LIMIT 1";
 		}
 		else {
-			$queryString = "SELECT usr, email FROM users WHERE usr LIKE '" . $usr . "' OR email = '" . $email . "' GROUP BY usr LIMIT 1";
+			$queryString = "SELECT usr, email FROM users WHERE usr = '" . $usr . "' OR LOWER(usr) = LOWER('" . $usr . "') OR email = '" . $email . "' GROUP BY usr LIMIT 1";
 		}
 
 		$q = $db->query($queryString);
 		$result = $q->fetch_assoc();
 
-		/*
 		if ($q->num_rows > 0) {
 			$appendage = "";
-			if (($res_usr != $usr) && (strtolower($res_usr) == strtolower($usr))) {
+			if (($result["usr"] != $usr) && (strtolower($result["usr"]) == strtolower($usr))) {
 				$appendage = ", in different case,";
 			}
 			return array(false, "An account with this username" . $appendage . " already exists");
 		}
-		elseif ($res_email !== NULL) {
+		elseif ($result["email"] !== NULL) {
 			return array(false, "An account with this email already exists");
 		}
-		*/
-		
-		//if ($q->num_rows > 0) {
-			return array(false, "ERROR");
-		//}
 
-		return array(true, "success");
+		return array(true, "");
 	}
 
 	function register($usr, $email, $passwd) {
 		global $db;
 
-		if (isAccount($usr)[0] == false) {
-			return isAccount($usr);
+		if (isAccount($usr)[0] === false) {
+			return isAccount($usr)[1];
 		}
-		if (isAccount($email)[0] == false) {
-			return isAccount(NULL, $email);
+		if (isAccount(NULL, $email)[0] === false) {
+			return isAccount(NULL, $email)[1];
 		}
-		echo "PASSED CHECKS";
-		
+
 		$passwd = password_hash($passwd, PASSWORD_BCRYPT);
 
-		$statement = $db->prepare("INSERT INTO users (usr, email, passwd, registered_on) VALUES (?, ?, ?, CURRENT_TIMESTAMP)");
-		$statement->bind_param("sss", $usr, $email, $passwd);
+		$statement = $db->prepare("INSERT INTO users (usr, email, passwd, registered_on, ip) VALUES (?, ?, ?, CURRENT_TIMESTAMP, ?)");
+		$statement->bind_param("ssss", $usr, $email, $passwd, $_SERVER["REMOTE_ADDR"]);
 		$statement->execute();
 
 		return true;
@@ -65,7 +58,7 @@
 
 		$regis = register($usr, $email, $passwd);
 		//var_dump("REGIS -> " . $regis);
-		if ($regis[1] !== true) {
+		if ($regis !== true) {
 			// Tell them the error
 			?>
 			<div class="text-center">
@@ -74,7 +67,7 @@
 					<div class="row">
 						<div class="text-center col-sm-6 col-md-4 col-md-offset-4">
 							<!--<font color="red"></h5></font>-->
-							<div class="alert alert-danger" role="alert"><h5><?php var_dump($regis); ?></div>
+							<div class="alert alert-danger" role="alert"><h5><?php echo($regis); ?></div>
 						</div>
 					</div>
 				</div>
@@ -84,8 +77,9 @@
 			include "src/templates/register.html";
 		}
 		else {
-			// Redirect them to the login page
-			header("Location: login.php");
+			// Redirect them to the index page, and log them in
+			$_SESSION["usr"] = $usr;
+			header("Location: index.php");
 		}
 	}
 	else {
