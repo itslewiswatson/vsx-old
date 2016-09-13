4147eeda3d5ba5dda6febf666bd1cde93194aec2
nokizorque <noki@zorque.xyz>
2016-09-13 21:55:14 +1000

    Added the logic from onEditSell to onEditBuy

--- src/js/onStockChange.js
+++ src/js/onStockChange.js
@@ -1,10 +1,10 @@
 Number.prototype.formatMoney = function(c, d, t) {
-	var n = this, 
-    c = isNaN(c = Math.abs(c)) ? 2 : c, 
-    d = d == undefined ? "." : d, 
-    t = t == undefined ? "," : t, 
-    s = n < 0 ? "-" : "", 
-    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+	var n = this,
+    c = isNaN(c = Math.abs(c)) ? 2 : c,
+    d = d == undefined ? "." : d,
+    t = t == undefined ? "," : t,
+    s = n < 0 ? "-" : "",
+    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
     j = (j = i.length) > 3 ? j % 3 : 0;
 	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
@@ -55,19 +55,41 @@ function onEditBuy() {
 		document.getElementById("buy-text").innerHTML = "Live stock price could not be fetched";
 		return;
 	}
-    var qty = document.getElementById("buy").value;
-	if (qty.replace(" ", "") == "") {
+
+	var qty = document.getElementById("buy").value;
+	if (qty == "" || qty.replace(" ", "") == "" || qty.length == 0) {
 		document.getElementById("buy-text").innerHTML = "";
 		return;
 	}
-    qty = Number(qty);
-    if (!isValidNumber(qty)) {
-        document.getElementById("buy-text").innerHTML = "Please enter a valid number";
-        return;
-    }
-	var price = qty * stockPrice;
+
+	qty = qty.replace(".", "").replace(",", "");
+	qty = Number(qty);
+	if (!isValidNumber(qty) || qty.NaN || qty == "Infinity") {
+		document.getElementById("buy-text").innerHTML = "Please enter a valid number";
+		document.getElementById("buy").value = "";
+		return;
+	}
+
+	var val = document.getElementById("buy").value; // Not casted to a number
+	while ((val.match(/,/g) || []).length) {
+		val = val.replace(",", "")
+	}
+
+	var val2 = Number(val); // val2 because we will use val for caret index
+	if (!val2 || isNaN(val2)) {
+		document.getElementById("buy-text").innerHTML = "Please enter a valid number";
+		document.getElementById("buy").value = "";
+		return;
+	}
+
+	if (document.getElementById("buy").value.replace(" ", "") == "") {
+		document.getElementById("buy-text").innerHTML = "";
+	}
+
+	var price = val2 * stockPrice;
 	price = (price).formatMoney(2);
-    document.getElementById("buy-text").innerHTML = "This will cost you <strong>$" + price + "</strong>";
+	document.getElementById("buy-text").innerHTML = "You will cost you <strong>$" + price + "</strong>";
+	document.getElementById("buy").value = tocomma(val2);
 }
 
 function onEditSell() {
@@ -75,13 +97,13 @@ function onEditSell() {
 		document.getElementById("sell-text").innerHTML = "Live stock price could not be fetched";
 		return;
 	}
-	
+
     var qty = document.getElementById("sell").value;
 	if (qty == "" || qty.replace(" ", "") == "" || qty.length == 0) {
 		document.getElementById("sell-text").innerHTML = "";
 		return;
 	}
-	
+
 	qty = qty.replace(".", "").replace(",", "");
     qty = Number(qty);
     if (!isValidNumber(qty) || qty.NaN || qty == "Infinity") {
@@ -89,23 +111,23 @@ function onEditSell() {
 		document.getElementById("sell").value = "";
         return;
     }
-	
+
 	var val = document.getElementById("sell").value; // Not casted to a number
 	while ((val.match(/,/g) || []).length) {
 		val = val.replace(",", "")
 	}
-	
+
 	var val2 = Number(val); // val2 because we will use val for caret index
 	if (!val2 || isNaN(val2)) {
 		document.getElementById("sell-text").innerHTML = "Please enter a valid number";
 		document.getElementById("sell").value = "";
 		return;
 	}
-	
+
 	if (document.getElementById("sell").value.replace(" ", "") == "") {
 		document.getElementById("sell-text").innerHTML = "";
 	}
-	
+
 	var price = val2 * stockPrice;
 	price = (price).formatMoney(2);
     document.getElementById("sell-text").innerHTML = "You will gain <strong>$" + price + "</strong>";
